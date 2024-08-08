10dc0744e53932e28af65e089e85e17a30769c9f
KLFzx <kliffa7@gmail.com>
2024-08-09 02:55:41 +0300

    new images

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import './stylesNavbar.css';
-import OverlayBox from './OverlayBox';
+import React from "react";
+import "./stylesNavbar.css";
+import OverlayBox from "./OverlayBox";
 
-import { CSSProperties } from 'react';
+import { CSSProperties } from "react";
 
 const Navbar: React.FC<{ id: number }> = ({ id }) => {
   return (
-    <nav className='fixed top-0 right-0  z-50 flex flex-row justify-between pr-10 pt-10 space-x-8 text-1xl font-regular'>
-      <div className='group'>
+    <nav className="fixed top-0 right-0  z-50 flex flex-row justify-between pr-10 pt-10 space-x-8 text-1xl font-regular">
+      <div className="group">
         <a
           className={`hover group-hover ${
-            id == 1 ? 'text-red-600' : 'text-white'
+            id == 1 ? "text-red-600" : "text-white"
           } `}
         >
           The Story
@@ -19,16 +19,26 @@ const Navbar: React.FC<{ id: number }> = ({ id }) => {
           <p>Coming soon...</p>
         </OverlayBox>
       </div>
-      <a
-        className={`hover ${id == 2 ? 'text-red-600' : 'text-white'} `}
-        href='/discord'
-      >
-        Discord
-      </a>
-      <div className='group'>
+      <div className="flex flex-col items-center ">
+        <a
+          className={`flex items-center justify-center flex-row hover ${
+            id == 2 ? "text-red-600" : "text-white"
+          } `}
+          href="/discord"
+        >
+          Discord
+        </a>
+        <img
+          src="/discord/discord.png"
+          alt="Discord Logo"
+          width={50}
+          height={50}
+        />
+      </div>
+      <div className="group">
         <a
           className={`hover group-hover ${
-            id == 3 ? 'text-red-600' : 'text-white'
+            id == 3 ? "text-red-600" : "text-white"
           } `}
         >
           Mods
@@ -37,10 +47,10 @@ const Navbar: React.FC<{ id: number }> = ({ id }) => {
           <p>Coming soon...</p>
         </OverlayBox>
       </div>
-      <div className='group'>
+      <div className="group">
         <a
           className={`hover group-hover ${
-            id == 4 ? 'text-red-600' : 'text-white'
+            id == 4 ? "text-red-600" : "text-white"
           } `}
         >
           Wiki
@@ -50,8 +60,8 @@ const Navbar: React.FC<{ id: number }> = ({ id }) => {
         </OverlayBox>
       </div>
       <a
-        className={`hover ${id == 5 ? 'text-red-600' : 'text-white'} `}
-        href='/'
+        className={`hover ${id == 5 ? "text-red-600" : "text-white"} `}
+        href="/"
       >
         Play now
       </a>
