9754ba6a67e84acef409d3ce9b9d2b5ff4c81f63
KLFzx <kliffa7@gmail.com>
2024-08-12 01:07:46 +0300

    improved scaling

--- components/ImageView.tsx
+++ components/ImageView.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { CSSProperties } from 'react';
-import Image from 'next/image';
+import React, { useState } from "react";
+import { CSSProperties } from "react";
+import Image from "next/image";
 const outline: CSSProperties = {
-  outline: '1px solid rgba(211, 211, 211, 0.2)',
+  outline: "1px solid rgba(211, 211, 211, 0.2)",
 
-  outlineOffset: '-10px',
+  outlineOffset: "-10px",
 };
 
 const ImageView: React.FC<{
@@ -23,57 +23,57 @@ const ImageView: React.FC<{
 
   return (
     <div
-      className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50 '
+      className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50 "
       style={outline}
     >
       <div
-        className='flex justify-start items-end h-screen w-screen'
+        className="flex justify-start items-end h-screen w-screen"
         style={{
           backgroundImage: `url(${background})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          transition: 'background-image 0.5s ease',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          transition: "background-image 0.5s ease",
         }}
       >
         <Image
-          src={'/icons/Cross.svg'}
+          src={"/icons/Cross.svg"}
           width={30}
           height={30}
-          alt={'X'}
-          className='absolute right-10 top-10 cursor-pointer'
+          alt={"X"}
+          className="absolute right-10 top-10 cursor-pointer"
           onClick={onClose}
         ></Image>
-        <div className='ml-[3rem] mb-[3rem]'>
-          <div className='bg-black mb-[3rem] bg-opacity-50 p-4 max-w-[45rem]'>
-            <h1 className=' text-3xl mb-4'>{title}</h1>
-            <p className='text-sm font-thin'>{desc}</p>
+        <div className="ml-[3rem] mb-[3rem]">
+          <div className="bg-black mb-[3rem] bg-opacity-50 p-4 max-w-[45rem]">
+            <h1 className=" text-3xl mb-4">{title}</h1>
+            <p className="text-sm font-thin">{desc}</p>
           </div>
-          <div className='flex gap-8'>
+          <div className="flex gap-8">
             <Image
               src={image1}
               height={140}
               width={240}
-              alt={''}
+              alt={""}
               style={outline}
-              className='cursor-pointer w-16 md:w-24 lg:w-[14rem] xl:w-[18rem] 2xl:w-[30rem]'
+              className="cursor-pointer w-16 sm:w-18 md:w-24 lg:w-[10rem] xl:w-[18rem] 2xl:w-[20rem] 3xl:w-[30rem] 4xl:w-[40rem] transform transition-transform duration-300 hover:scale-105 "
               onClick={() => changeBackground(image1)}
             ></Image>
             <Image
               src={image2}
               height={140}
               width={240}
-              alt={''}
+              alt={""}
               style={outline}
-              className='cursor-pointer w-16 md:w-24 lg:w-[14rem] xl:w-[18rem] 2xl:w-[30rem]'
+              className="cursor-pointer w-16 sm:w-18 md:w-24 lg:w-[10rem] xl:w-[18rem] 2xl:w-[20rem] 3xl:w-[30rem] 4xl:w-[40rem] transform transition-transform duration-300 hover:scale-105 "
               onClick={() => changeBackground(image2)}
             ></Image>
             <Image
               src={image3}
               height={140}
               width={240}
-              alt={''}
+              alt={""}
               style={outline}
-              className='cursor-pointer w-16 md:w-24 lg:w-[14rem] xl:w-[18rem] 2xl:w-[30rem]'
+              className="cursor-pointer w-16 sm:w-18 md:w-24 lg:w-[10rem] xl:w-[18rem] 2xl:w-[20rem] 3xl:w-[30rem] 4xl:w-[40rem] transform transition-transform duration-300 hover:scale-105 "
               onClick={() => changeBackground(image3)}
             ></Image>
           </div>
